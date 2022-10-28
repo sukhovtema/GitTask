8b14e0c3f18703a6cecf979de713e0ddff83d29c
Владимир <vladimir-ratnikov2002@mail.ru>
2022-10-28 18:14:18 +0300

    First Change

--- GitTask/Program.cs
+++ GitTask/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+
+// сортировка
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+
+// вывод
+Console.WriteLine("Вывод отсортированного массива");
